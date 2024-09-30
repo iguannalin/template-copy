173ca6c40600e512ef094cbd5f224ea74f7c5a85
seasons986 <annalin12345@gmail.com>
2024-09-30 14:56:55 -0400

    style updates

--- index.js
+++ index.js
@@ -4,5 +4,11 @@ window.addEventListener("load", () => {
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min) + min); // The maximum is exclusive and the minimum is inclusive
   }
-  const container = document.getElementById("container");
+  const container = document.querySelector(".container");
+  const imageContainer = document.querySelector(".container-image");
+  let delta = -1;
+  setInterval(() => {
+    if (+(imageContainer.style.opacity) == 1 || +(imageContainer.style.opacity) == 0) delta *= -1;
+    imageContainer.style.opacity = +(imageContainer.style.opacity) + (delta * 0.05);
+  }, 200);
 });
